0de780cd61cc48fa54dc004c025065cfa0b36ace
NanGuoShaoNian <18851170455@163.com>
2017-12-03 18:17:20 +0800

    商品分类

---
+++ fhliuzhihu-manager-web/src/main/java/cn/sunshineShop/controller/ItemCatController.java
@@ -0,0 +1,35 @@
+package cn.sunshineShop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.sunshineShop.common.bean.EUTreeNode;
+import cn.sunshineShop.service.ItemCatService;
+
+/**
+ * 商品分类管理controller
+ * 
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("/item/cat")
+public class ItemCatController {
+
+	@Autowired
+	private ItemCatService itemCatService;
+
+	@RequestMapping("/list")
+	@ResponseBody
+	private List<EUTreeNode> getCatList(@RequestParam(value = "id", defaultValue = "0") Long parentId) {
+		List<EUTreeNode> list = itemCatService.getItemCatList(parentId);
+		return list;
+	}
+}
+
+
---
+++ fhliuzhihu-manager-web/src/main/java/cn/sunshineShop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package cn.sunshineShop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.sunshineShop.common.bean.EUDataGridResult;
+import cn.sunshineShop.pojo.TbItem;
+import cn.sunshineShop.service.ItemService;
+
+/***
+ * 商品管理Controller
+ * @author Administrator
+ *
+ */
+
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EUDataGridResult getItemList(Integer page, Integer rows) {
+		EUDataGridResult result = itemService.getItemList(page, rows);
+		return result;
+	}
+}
